c04a349eee4cbc2ac1d3161406e107bc3c245f64
Oksanamarusich <oksana.m229@gmail.com>
2023-10-21 23:46:12 +0300

    bagfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,26 +4,16 @@ import { nanoid } from 'nanoid';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-const getInitialContacts = () => {
-  const contactsList = localStorage.getItem('contacts');
- console.log(contactsList)
-  if (contactsList !== []) {
-    return JSON.parse(contactsList);
-  } else if (contactsList === []) {
-     return {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-       ]
-       
-  };
-  };
-};
 
 export const App = () => {
-  const [contacts, setContacts] = useState(getInitialContacts);
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ||
+      [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},]
+  );
+   
   const [filter, setFilter] = useState('');
   
   
@@ -47,14 +37,19 @@ export const App = () => {
    setFilter(newFilter);
   }
 
-  const visibleName = contacts.filter(contact =>
-       contact.name.toLowerCase().includes(filter.toLowerCase()));
-   
+ const getVisibleName = () => {
+    
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()));
+  }
+
  const deleteContacts = contactId => {
     setContacts(prevContacts => contacts.filter(contact=>contact.id !== contactId ))
-}
-      
-return (<div>
+  }
+
+  const visibleName = getVisibleName();
+  
+  return (<div>
        <h1>Phonebook</h1>
        <ContactForm onAdd={addContacts } />
       
